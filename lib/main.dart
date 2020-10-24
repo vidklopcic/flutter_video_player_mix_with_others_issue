4c03f425eb6bff1d260019e204141ba9e7a11f16
Vid Klopcic <klopcic.vid@gmail.com>
2020-10-24 13:58:24 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,3 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: public_member_api_docs
-
-/// An example of using the plugin, controlling lifecycle and playback of the
-/// video.
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
